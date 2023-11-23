c860dbfead6384e37776420abe3a263d62a451d5
Abdulaziz <abdulazizmukhtarov22@gmail.com>
2023-11-23 13:52:03 +0500

    no

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,16 @@
 
 #include <iostream>
 #include "task_1.h"
+#include "isPowerOfTwo.h"
+#include "sumofdigits.h"
 
 int main() {
     std::cout << "Task 1" << std::endl;
-    // call for task 1
+    task_1()
     std::cout << "Task 2" << std::endl;
-    // call for task 2
+    isPowerOfTwo()
     std::cout << "Task 3" << std::endl;
-    // call for task 3
+    sumofdigits()
     std::cout << "Task 4" << std::endl;
     // call for task 4
     std::cout << "Task 5" << std::endl;
---
+++ src/sumofdigits.h
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+// Function to calculate the sum of digits of a number using recursion
+// Time complexity: O(log N), where N is the input number
+int sumOfDigits(int N) {
+    // Base case: if N is less than 10, return N
+    if(N < 10) {
+        return N;
+    }
+    // Recursive case: return the last digit of N plus the sum of the digits of N/10
+    else {
+        return N % 10 + sumOfDigits(N / 10);
+    }
+}
+
+int sumofdigits() {
+    int N;
+    cout << "Enter a natural number N: ";
+    cin >> N;
+    cout << "The sum of the digits of " << N << " is " << sumOfDigits(N) << endl;
+    return 0;
+}
